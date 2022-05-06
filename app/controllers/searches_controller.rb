4348338a4221c54358a22efbd5dd7e3c5fb5e97a
aokazuevirob <102888386+aokazuevirob@users.noreply.github.com>
2022-05-06 10:17:38 +0000

    [Finish]応用課題5 検索機能追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,18 +3,12 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
+    @word = params[:word]
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
-      redirect_to search_result_path(@users)
     else
       @books = Book.looks(params[:search], params[:word])
-      redirect_to search_result_path(@books)
     end
-  end
-
-  def search_result
-    @users = User.all
-    @books = Book.all
   end
 
 end
